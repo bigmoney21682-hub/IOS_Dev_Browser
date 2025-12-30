82b0536bbdf1f88784d0da2f7ad0d0f9ddc0b285
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-30 11:41:11 -0500

    Create DebugBrowserApp.swift

---
+++ ios/DebugBrowserApp/DebugBrowserApp.swift
@@ -0,0 +1,15 @@
+// FILE: DebugBrowserApp.swift
+// PATH: ios/DebugBrowserApp/
+// DESC: App entry point for IOS Dev Browser (SwiftUI lifecycle)
+
+import SwiftUI
+
+@main
+struct DebugBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
