498c572ddf4e199f8a9924f782b3d2f6ddc29bad
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-30 11:49:15 -0500

    Create Constants.swift

---
+++ ios/Shared/Constants.swift
@@ -0,0 +1,10 @@
+// FILE: Constants.swift
+// PATH: ios/Shared/
+// DESC: Global constants for IOS Dev Browser
+
+import Foundation
+
+struct Constants {
+    static let appName = "IOS Dev Browser"
+    static let version = "0.1.0"
+}
