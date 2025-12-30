5c9009a9d150c79fca1aac353141f1f13e3fa4a5
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-30 14:08:31 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,5 @@
 // PATH: /service-worker.js
 // DESC: Minimal service worker for PWA installability
 
-self.addEventListener("install", (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-    clients.claim();
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => clients.claim());
