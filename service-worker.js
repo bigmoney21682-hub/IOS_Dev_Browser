e95e13e7eaeef4e38ad5e2b8bb9097bee7f59659
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-30 15:11:04 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,9 @@
+// ============================================================
 // FILE: service-worker.js
 // PATH: /service-worker.js
-// DESC: Minimal service worker for PWA installability
+// DESC: Minimal service worker to enable PWA installability and
+//       immediate activation for IOS Dev Browser.
+// ============================================================
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => clients.claim());
